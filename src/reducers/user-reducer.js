d82b18baa4793f5fc36bc649db6c1c3e50da5abe
Emrah <emrahyurttutan@gmail.com>
2018-10-01 17:19:25 +0300

    form-validator eklendi

--- src/reducers/user-reducer.js
+++ src/reducers/user-reducer.js
@@ -1,7 +1,7 @@
 import { IS_LOGIN, USER_NAME, USER_PASSWORD } from "../actions/action-type";
 
 const initialState = {
-  username: "",
+  username: "emrah",
   password: "",
   loading: false,
   success: false,
